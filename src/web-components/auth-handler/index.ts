d395ad666446ed429347e3f31962888f9759b38f
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-05-22 13:39:43 +0900

    add login logic

--- src/web-components/auth-handler/index.ts
+++ src/web-components/auth-handler/index.ts
@@ -1,4 +1,6 @@
 import template from "./template.html";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase";
 
 interface FormData {
   email: string;
@@ -13,10 +15,12 @@ export default class AuthHandler extends HTMLElement {
   #registerButton: HTMLButtonElement;
   #form: HTMLFormElement;
   #error: string | null;
+  #loading: boolean;
 
   constructor() {
     super();
     this.#error = null;
+    this.#loading = false;
     this.attachShadow({ mode: "open" });
     if (!this.shadowRoot) throw Error("Shadow root not attached.");
     this.shadowRoot.innerHTML = template;
@@ -33,14 +37,22 @@ export default class AuthHandler extends HTMLElement {
   }
 
   get error(): string | null {
-    return this.#error
+    return this.#error;
   }
-  set error(value :string | null) {
+  set error(value: string | null) {
     this.#error = value;
     const errorElement = this.shadowRoot?.getElementById("errors");
-    if (!errorElement) throw Error("Error element not found.")
+    if (!errorElement) throw Error("Error element not found.");
     errorElement.toggleAttribute("show", Boolean(this.#error));
     errorElement.textContent = this.#error;
+  }
+  get loading(): boolean {
+    return this.#loading;
+  }
+  set loading(value: boolean) {
+    this.#loading = value;
+    const button = this.shadowRoot?.getElementById("submit");
+    button!.textContent = this.#loading ? "Loading..." : "Submit";
   }
 
   disconnectedCallback() {
@@ -49,22 +61,47 @@ export default class AuthHandler extends HTMLElement {
     this.#form.removeEventListener("submit", this.#handleSubmit);
   }
 
-  #handleSubmit: EventListener = (event) => {
+  #handleSubmit: EventListener = event => {
     event.preventDefault();
     this.error = null;
     const formData = new FormData(this.#form);
-    const formDataObj = Object.fromEntries(formData) as unknown
+    const formDataObj = Object.fromEntries(formData) as unknown;
     const data = formDataObj as FormData;
     const isRegister = Boolean(data["password-confirm"]);
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(data.email)) {
+      this.error = "Must provide valid Email.";
+    }
     if (isRegister) {
       const passwordsMatch = data.password === data["password-confirm"];
       if (!passwordsMatch) {
-        this.error = "Passwords do not Match."
+        this.error = "Passwords do not Match.";
         return;
       }
-      return
+      this.loading = true;
+      createUserWithEmailAndPassword(auth, data.email, data.password)
+        .then(credentials => {
+          this.removeAttribute("show");
+          this.#form.reset();
+        })
+        .catch(error => {
+          if (!(error instanceof Error)) return;
+          this.error = error.message;
+        })
+        .finally(() => (this.loading = false));
+    } else {
+      this.loading = true;
+      signInWithEmailAndPassword(auth, data.email, data.password)
+        .then(() => {
+          this.removeAttribute("show");
+          this.#form.reset();
+        })
+        .catch(error => {
+          if (!(error instanceof Error)) return;
+          this.error = error.message;
+        })
+        .finally(() => (this.loading = false));
     }
-  }
+  };
 
   #setForm = (mode: "login" | "register") => {
     const template = mode === "login" ? this.#loginTemplate : this.#registerTemplate;
