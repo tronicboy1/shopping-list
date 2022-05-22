7d7d364e6374290ee146c6751a69470163ceb01d
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-05-22 13:56:46 +0900

    remove auth from react

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import firebase from "./services/firebase";
 import ContextProvider from "./helpers/ContextProvider";
 import BaseCard from "@web-components/base-card";
 import AuthHandler from "@web-components/auth-handler";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 customElements.define("auth-handler", AuthHandler);
 
@@ -13,10 +15,19 @@ if (!rootElement) throw Error("Root element not found.");
 
 const root = createRoot(rootElement);
 
-customElements.define("base-card", BaseCard)
+customElements.define("base-card", BaseCard);
 
-root.render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>
-);
+const auth = getAuth(firebase);
+onAuthStateChanged(auth, user => {
+  const authHandler = document.querySelector("auth-handler") as AuthHandler;
+  if (user) {
+    authHandler.toggleAttribute("show", false);
+    root.render(
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    );
+  } else {
+    authHandler.toggleAttribute("show", true);
+  }
+});
