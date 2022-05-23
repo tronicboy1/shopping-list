8fed4f6c0f4850e028fc7505ad028b560594ea1e
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-05-24 07:17:27 +0900

    fix style for safari

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import AuthHandler from "@web-components/auth-handler";
 import ShoppingList from "@web-components/shopping-list";
+import "./index.css";
 
 customElements.define("auth-handler", AuthHandler);
 customElements.define("shopping-list", ShoppingList);
