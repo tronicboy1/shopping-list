fbbd71370297114ba400f3c43109d112aaa8cf59
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-05-24 06:52:37 +0900

    setup web component list

--- src/web-components/shopping-list/index.ts
+++ src/web-components/shopping-list/index.ts
@@ -1,6 +1,6 @@
 import template from "./template.html";
 import firebase from "../../services/firebase";
-import { getDatabase, ref, onValue, set, DatabaseReference } from "firebase/database";
+import { getDatabase, ref, onValue, set, DatabaseReference, push } from "firebase/database";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 interface ShoppingListData {
@@ -14,6 +14,9 @@ export default class ShoppingList extends HTMLElement {
   #data: ShoppingListData | null;
   #ref: DatabaseReference | null;
   #clicked: string | null;
+  #input: HTMLInputElement;
+  #form: HTMLFormElement;
+  #clearAll: HTMLButtonElement;
 
   constructor() {
     super();
@@ -24,6 +27,12 @@ export default class ShoppingList extends HTMLElement {
     if (!this.shadowRoot) throw Error("Shadow root not attached");
     this.shadowRoot.innerHTML = template;
     this.#list = this.shadowRoot.querySelector("ul")!;
+    this.#input = this.shadowRoot.querySelector("input")!;
+    this.#form = this.shadowRoot.querySelector("form")!;
+    this.#clearAll = this.shadowRoot.getElementById("clear") as HTMLButtonElement;
+    this.#input.addEventListener("input", this.#handleInput);
+    this.#form.addEventListener("submit", this.#handleAddItem);
+    this.#clearAll.addEventListener("click", this.#handleItemClick);
   }
 
   connectedCallback() {
@@ -35,6 +44,7 @@ export default class ShoppingList extends HTMLElement {
         this.#ref = ref(db, `${auth.uid}/SHOPPING/`);
         onValue(this.#ref, snapshot => {
           this.#data = snapshot.val() as ShoppingListData;
+          this.#list.childNodes.forEach(li => li.removeEventListener("click", this.#handleItemClick));
           this.#list.innerHTML = "";
           if (this.#data) {
             for (const key in this.#data) {
@@ -42,7 +52,7 @@ export default class ShoppingList extends HTMLElement {
               const li = document.createElement("li");
               li.textContent = text;
               li.id = key;
-              li.addEventListener("click", this.#handleClick);
+              li.addEventListener("click", this.#handleItemClick);
               this.#list.append(li);
             }
           } else {
@@ -55,12 +65,19 @@ export default class ShoppingList extends HTMLElement {
     });
   }
 
-  #handleClick: EventListener = event => {
+  dissconnectedCallback() {
+    this.#input.removeEventListener("input", this.#handleInput);
+    this.#form.removeEventListener("submit", this.#handleAddItem);
+    this.#clearAll.removeEventListener("click", this.#handleItemClick);
+  }
+
+  #handleItemClick: EventListener = event => {
     const target = event.target;
-    if (!(target instanceof HTMLLIElement)) return;
+    if (!(target instanceof HTMLLIElement || target instanceof HTMLButtonElement)) return;
     const id = target.id;
     if (this.#clicked === id) {
-      this.#deleteItem(id);
+      console.log(id);
+      id === "clear" ? this.#deleteAllItems() : this.#deleteItem(id);
       this.#clicked = null;
     } else {
       this.#clicked = id;
@@ -73,5 +90,23 @@ export default class ShoppingList extends HTMLElement {
     if (!(this.#data && this.#ref)) return;
     delete this.#data[id];
     set(this.#ref, this.#data);
+  };
+  #deleteAllItems = () => {
+    console.log("delete all");
+    set(this.#ref!, {});
+  };
+
+  #handleInput: EventListener = event => {
+    if (this.#input.value.length === this.#input.maxLength) {
+      this.#input.setAttribute("class", "invalid");
+    } else {
+      this.#input.hasAttribute("class") && this.#input.removeAttribute("class");
+    }
+  };
+  #handleAddItem: EventListener = event => {
+    event.preventDefault();
+    const item = this.#input.value.trim();
+    push(this.#ref!, { item });
+    this.#form.reset();
   };
 }
