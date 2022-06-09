d5a289f477521a696f8985c93b098b415b11d4ed
Austin Mayer <a.inoue@churadata.okinawa>
2022-06-09 17:03:09 +0900

    simplify backend not logic

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,11 @@ import { getDatabase } from "firebase-admin/database";
 import { getMessaging } from "firebase-admin/messaging";
 const key = require("./shopping-list-app-d0386-firebase-adminsdk-4n6jo-97fc02004b.json");
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.write("OK");
+  res.statusCode = 200;
+  return res.end();
+});
 
 admin.initializeApp({
   credential: admin.credential.cert(key),
@@ -15,31 +19,24 @@ const messaging = getMessaging();
 
 const db = getDatabase();
 
-type FCMKeys = Record<string, string[]>;
-let fcmKeys: FCMKeys = {};
-const fcmRef = db.ref("/FCM/");
-fcmRef.on("value", (snapshot) => {
-  const value = snapshot.val() as FCMKeys | null;
-  if (!(value && value instanceof Object)) return;
-  fcmKeys = value;
-});
-
 const ref = db.ref("/NOTIFICATIONS/");
 ref.on("child_changed", (snapshot) => {
   const newNotification = snapshot.val() as { item?: string; uid: string } | null;
-  const uidList = Object.keys(fcmKeys);
-  if (!(uidList.length && newNotification)) return;
-  const fcmTokens = fcmKeys[newNotification.uid];
-  if (!fcmTokens || fcmTokens.length === 0) return;
-  console.log(newNotification, fcmTokens);
-  messaging
-    .sendToDevice(fcmTokens, {
-      notification: {
-        title: "List Updated",
-        body: `New Item ${newNotification.item} was updated in your list.`,
-      },
-    })
-    .then((value) => console.log("Notification Sent", value));
+  if (!newNotification) return;
+  const fcmRef = db.ref(`/FCM/${newNotification.uid}`);
+  fcmRef.get().then((fcmTokensData) => {
+    const fcmTokens = fcmTokensData.val() as string[] | null;
+    if (!fcmTokens || fcmTokens.length === 0) return;
+    messaging
+      .sendToDevice(fcmTokens, {
+        notification: {
+          title: "List Updated",
+          body: `New Item ${newNotification.item} was updated in your list.`,
+        },
+      })
+      .then((value) => console.log("Notification Sent", newNotification.uid));
+  });
 });
 
-server.listen(4000, () => console.log("listening on 4000"));
+const port = 8080;
+server.listen(port, () => console.log(`listening on ${port}`));
