97b028daf4db9ef812a86c0ab2c31d190b49e8f6
Austin Mayer <a.inoue@churadata.okinawa>
2022-06-10 10:48:47 +0900

    add firebase app check

--- src/web-components/firebase.ts
+++ src/web-components/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBZ3KUebo7OAtHJQRwEJr2VEpH1yWktahE",
@@ -8,9 +9,18 @@ export const firebaseConfig = {
   projectId: "shopping-list-app-d0386",
   storageBucket: "shopping-list-app-d0386.appspot.com",
   messagingSenderId: "302654429160",
-  appId: "1:302654429160:web:b1d02796a6b4d7fa92365d"
+  appId: "1:302654429160:web:b1d02796a6b4d7fa92365d",
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
+
+if (process.env.NODE_ENV === "development") {
+  //@ts-ignore
+  self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+}
+export const appCheck = initializeAppCheck(firebaseApp, {
+  provider: new ReCaptchaV3Provider("6LcuLlsgAAAAADL_n_1hS7zeQMKX6xbi10jQYIYR"),
+  isTokenAutoRefreshEnabled: true,
+});
 
 export const auth = getAuth(firebaseApp);
